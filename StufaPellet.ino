9449920e4d094a108a84d49b90846ce5f6dec2c8
Tolentino Cotesta <cotestatnt@yahoo.com>
2019-05-12 10:44:41 +0200

    Update StufaPellet.ino

--- StufaPellet.ino
+++ StufaPellet.ino
@@ -8,18 +8,21 @@
 
 #define OFF LOW
 #define ON HIGH
-#define TELEGRAM_TIME 500
-#define UPDATE_TEMP_TIME 1000
-#define CHECK_ON_TIME 20000
-#define ON_TEMP 45.0F
+#define TELEGRAM_TIME 500       // Intervallo di tempo con cui vengono controllati i messaggi
+#define UPDATE_TEMP_TIME 1000   // Intervalo di tempo per aggiornare la temperatura
+#define CHECK_ON_TIME 20000     // CHECK_ON_TIME ms dopo aver attivato la caldaia, eseguo una verifica sulla temperatura raggiunta
+#define ON_TEMP 45.0F           // Temperatura oltre la quale considero la caldaia partita correttamente
 
+// Messaggi di callback per Telegram a seguito di query
 #define START_CALLBACK  "Comando bruciatore ON" 
 #define STOP_CALLBACK "Comando bruciatore OFF"
 #define STATE_CALLBACK "Stato bruciatore"
 #define CONFIRM_CALLBACK "Esegui"
 #define CANCEL_CALLBACK "Annulla"
 
+// Enum degli stati finiti possibili
 typedef enum { WAIT = -1, STOP = 0, START = 1, STATE = 3};
+// Strutura per memorizzare messaggio Telegram
 typedef struct  {
   int request = WAIT;
   bool confirm = false;
@@ -27,6 +30,7 @@ typedef struct  {
   uint16_t value2;
 } telegramCmd;
 
+// Vars
 const uint8_t OUT = D1;    
 uint32_t checkTelegramTime, updateTempTime, checkTempTime; 
 uint8_t heatStatus = ON;
@@ -54,7 +58,9 @@ void setup() {
   Serial.begin(115200); 
   connectedHandler = WiFi.onStationModeConnected(&onConnection);
   disconnectedHandler = WiFi.onStationModeDisconnected(&onDisconnection);
-    
+  
+  // Avvio del webserver utie per memorizzare le credenziali del WiFi e le altre variabili
+  // come il token Telegram (TODO: inserire anche la temperatura ON, ed il tempo di attesa)
   WiFiManager wifiManager;  
   WiFiManagerParameter telegramToken("Telegram", "Token", "token_placeholder", 50);
   //wifiManager.resetSettings();
@@ -69,12 +75,15 @@ void setup() {
     WiFiEventStationModeConnected nullEvent;
     onConnection(nullEvent);
   }  
-  // Dallas DS18B20
+  
+  // Avvio gestione sensore di temperatura Dallas DS18B20
   sensors.begin();  
   Serial.printf("Locating devices...\nFound %d devices\n", sensors.getDeviceCount());
   if (!sensors.getAddress(ds18b20, 0)) Serial.println("Unable to find address for Device 0");
   sensors.setResolution(ds18b20, 10);
   sensors.requestTemperatures();
+  
+  // Se la temperatura attuale è maggiore di ON_TEMP considero la caldaia accesa
   ActualTemp = sensors.getTempC(ds18b20);
   if (ActualTemp > ON_TEMP)
     heatStatus = ON;      
@@ -83,19 +92,22 @@ void setup() {
 }
 
 void loop() { 
-  
+  // Controllo se è arrivato qualche messaggio da Telegram
   if(millis() - checkTelegramTime > TELEGRAM_TIME){
     checkTelegramTime = millis();
     checkTelegramKbd();
   }
   
+  // Aggiorno la temperatura attuale letta dal sensore 
   if(millis() - updateTempTime > UPDATE_TEMP_TIME){ 
     updateTempTime = millis();
     sensors.requestTemperatures();          
     ActualTemp = sensors.getTempC(ds18b20);     
     Serial.printf("Temperature: %02d.%02d°C\n", (int)ActualTemp, (int)(ActualTemp*100)%100);
   }
-
+  
+  // Se ho ricevuto il comando di START e sono trascorsi CHECK_ON_TIME ms, controllo la temperatura 
+  // e fornisco feedback all'utente con un messaggio Telegram
   if((millis() - checkTempTime > CHECK_ON_TIME) && (cmdMsg.request == START)){    
     char tx_buffer[80];
     if(ActualTemp > ON_TEMP){
@@ -113,10 +125,13 @@ void loop() {
       Serial.println(tx_buffer);
       heatStatus = OFF;
       cmdMsg.request = WAIT;      
-    }
-    
+    }    
   }
-
+  
+  /* Per evitare comandi accidentali, ogni variazione di stato ricevuta da Telegram va confermata.
+     Quindi alla richiesta di START ad esempio, invio un messaggio di richiesta conferma e solo quando
+     ricevo risposta positiva/negativa eseguo il comando previsto 
+  */
   if(cmdMsg.confirm == true){
     cmdMsg.confirm = false;
     if(cmdMsg.request == START && heatStatus == OFF){
@@ -133,11 +148,12 @@ void loop() {
   }
 }
 
+// Funzione di callback che viene eseguita al salvataggio della configurazione (WiFiManager)
 void saveConfigCallback() {
   Serial.println("Congigurazione salvata in EEPROM");  
 }
 
-
+// Funzione di callback che viene eseguita alla connessione ad una rete WiFi
 void onConnection(const WiFiEventStationModeConnected& evt) {
   Serial.println("\nWiFi connesso");     
   //myBot.wifiConnect(ssid, pass);
@@ -154,46 +170,53 @@ void onConnection(const WiFiEventStationModeConnected& evt) {
   goKbd.addButton("ANNULLA", CANCEL_CALLBACK, CTBotKeyboardButtonQuery);
 }
 
+// Funzione di callback che viene eseguita alla disconnessione da una rete WiFi
 void onDisconnection(const WiFiEventStationModeDisconnected& evt) {
   Serial.print("WiFi disconnected. Reason: ");  
   Serial.println(evt.reason); 
 }
 
-
+// Controllo dei messaggi ricevuti da Telegram
 void checkTelegramKbd(){
   //TBMessage msg;
   #define TX_SIZE 120
+  // Abiamo un nuovo messaggio?
   if (myBot.getNewMessage(msg)) {    
     char tx_buffer[TX_SIZE];
+    // E' un messaggio di testo qualsiasi
     if (msg.messageType == CTBotMessageText) {       
       sprintf(tx_buffer, "Ciao %s.!\nStato bruciatore : %s\nTemperatura acqua : %02d.%02d°C\n",
               msg.sender.username.c_str(), heatStatus ? "ON" : "OFF", (int)ActualTemp, (int)(ActualTemp * 100) % 100);
       myBot.sendMessage(msg.sender.id, tx_buffer, myKbd);
     } 
+    // E' una query, ovvero una richiesta da eseguire o un messaggio di conferma
     else if (msg.messageType == CTBotMessageQuery) {      
       if (msg.callbackQueryData.equals(START_CALLBACK)) {
-        cmdMsg.confirm = false;  
         cmdMsg.request = START;          
-        checkTempTime = millis();                
-        myBot.endQuery(msg.callbackQueryID, "Comando di accensione bruciatore ricevuto..");                   
+        checkTempTime = millis();
+        // Messaggio di "feedback" inviato alla ricezione della query
+        myBot.endQuery(msg.callbackQueryID, "Comando di accensione bruciatore ricevuto..");    
+        // Questo messaggio richiede conferma
+        cmdMsg.confirm = false; 
         myBot.sendMessage(msg.sender.id, "Confermi l'esecuzione del comando?", goKbd);  
       } 
       else if (msg.callbackQueryData.equals(STOP_CALLBACK)) {        
-        cmdMsg.confirm = false;                                   
         cmdMsg.request = STOP;    
-        myBot.endQuery(msg.callbackQueryID, "Comando di spegnimento bruciatore ricevuto..");               
+        myBot.endQuery(msg.callbackQueryID, "Comando di spegnimento bruciatore ricevuto..");
+        // Questo messaggio richiede conferma
+        cmdMsg.confirm = false; 
         myBot.sendMessage(msg.sender.id, "Confermi l'esecuzione del comando?", goKbd);              
       }
       else if (msg.callbackQueryData.equals(STATE_CALLBACK)) {
-        cmdMsg.confirm = false;        
+        cmdMsg.confirm = true;    // Questo messaggio non richiede conferma    
         sprintf(tx_buffer, "Stato bruciatore: %s\nTemperatura acqua: %02d.%02d°C\n",
                             heatStatus ? "ON" : "OFF", (int)ActualTemp, (int)(ActualTemp*100)%100);
         myBot.endQuery(msg.callbackQueryID, "");                                  
         myBot.sendMessage(msg.sender.id, tx_buffer);   
       }
       else if (msg.callbackQueryData.equals(CONFIRM_CALLBACK)) {
-        cmdMsg.confirm = true;                                      
-        checkTempTime = millis(); 
+        cmdMsg.confirm = true;      // Questo messaggio non richiede conferma                                      
+        checkTempTime = millis();   // Faccio partire il timer per controllo temperatura
         myBot.endQuery(msg.callbackQueryID, "");
         if(cmdMsg.request != WAIT)
           myBot.sendMessage(msg.sender.id, "Comando eseguito."); 
